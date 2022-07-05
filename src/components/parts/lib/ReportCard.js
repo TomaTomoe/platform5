a3c725b556c3ef2b548cd3d059aeff5bf0719ad3
Tina Denysiuk <denysiuk.tina@gmail.com>
2022-07-05 09:44:43 +0100

    to view pdf - not download at first

--- src/components/parts/lib/ReportCard.js
+++ src/components/parts/lib/ReportCard.js
@@ -5,7 +5,7 @@ import reportImage from "../../images/report-img.png";
 export default function ReportCard() {
     return (
         <article className="max-w-[326px] ">
-            <Link to="/reports/peron.pdf" target={"_blank"} download>
+            <Link to="/reports/peron.pdf" target={"_blank"}>
                 <img className="h-[186px] w-full" src={reportImage} alt="Report illustration"/>
                 <p className="text-2xl text-black font-medium pt-5 px-1">5 перон. Проєктний звіт</p>
             </Link>
