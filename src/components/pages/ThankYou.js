015d584c27e3537b22bc86c4f55697c82dc3bf1e
Tina Denysiuk <denysiuk.tina@gmail.com>
2022-09-09 12:05:31 +0100

    add img on thank_you page, fix description for donations, add alts

--- src/components/pages/ThankYou.js
+++ src/components/pages/ThankYou.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import thankYouImg from "../images/thank_you.jpg";
 
 export default function ThankYou() {
     return (
-        <main className="mt-[73px] md:mt-0 flex full-height items-center justify-items-center">
+        <main className="mt-[73px] md:mt-0 flex  items-center justify-items-center">
             <section id="thank_you" className="max-w-[1728px] mx-auto pt-9 pb-9 px-8
                 md:px-8 md:pt-12 md:pb-12
                 lg:px-16 lg:pt-16
@@ -17,6 +18,7 @@ export default function ThankYou() {
                     2xl:px-16 
                     3xl:px-32">Дякуємо за Вашу підтримку!</h2>
                 <div>
+                    <img className="mb-8 xl:mb-16 mx-auto w-full max-w-[600px]" src={thankYouImg} alt="Дякуємо за підтримку" />
                     <Link to="/#home"
                         className="block text-white bg-orange uppercase font-bold py-2.5 px-[40px] h-12 text-lg max-w-[270px] m-auto sm:py-5 sm:px-[50px] sm:h-16 sm:text-xl text-center"
                         >На головну</Link>
